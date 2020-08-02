2b3e1605ebfc6141ea445c934e22939779acfde3
Jose Silva <46269951+josesilva27@users.noreply.github.com>
2020-08-01 23:04:59 -0400

    Add files via upload

--- client.py
+++ client.py
@@ -5,12 +5,32 @@ import zipfile
 def main ():
     mi_socket = socket.socket()
     mi_socket.bind( ('localhost',8000) )
-    mi.socket.listen(5)
+    mi_socket.listen(5)
+ 
     
+    if os.path.exists("cliente")==True:             #compruebo que existe carpeta
+        ruta= os.getcwd()+"\cliente"
+        if len(os.listdir(ruta))== 0:               #compruebo si esta vacia
+            print("carpeta vacia")
+        else:
+            while True: 
+                conexion,addr = mi_socket.accept()
+                conexion.send("cliente conectado")
 
-    with zipfile.ZipFile('Prueba.zip','r') as myzip:
-    myzip.extractall()
+                file = open ('Prueba.zip','r')
+                input_data =conexion.recv(1024)
+                file.write (input_data)
+                conexion.send ("archivo descargado por el cliente")
+                break
 
+        with zipfile.ZipFile('Prueba.zip','r') as myzip:        #descomprimo el archivo ZIP
+            myzip.extractall()
+
+        conexion.close()
+        file.close()
+        print ("El archivo se ha descargado exitosamente")
+    else:
+        print("Crapeta servidor no cliente, cree la carpeta y llamela 'servidor' ")
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -6,32 +6,41 @@ def empaque ( path,zipActual ) :
     zipActual.write('LinkWarrior.jpg')
 
 def main ():
-    mi_socket = socket.socket()
-    mi_socket.bind( ('localhost',8000) )
-    mi.socket.listen(5)
-
     myzip = zipfile.ZipFile('Prueba.zip','w') #creo el paquete
     empaque(os.getcwd(),myzip)
     myzip.close()
-
-    conexion,addr = mi_socket.accept()      
-
-    while True:
-        print ("Nueva conexión establecida")
-        print (addr)
-
-        file = open ("Prueba.zip","rb")
-        content = file.read (1024)
-
-        while True:
-            conexion.send(content)      #envio contenido
-            content = file.read (1024)
-            conexion.send("descarga completa")
-        break
-
-    conexion.close()
-    file.close()
-    print ("El archivo se ha enviado exitosamente")
     
+    mi_socket = socket.socket()
+    mi_socket.bind( ('localhost',8000) )        #creo la conexion
+    mi_socket.listen(5)
+     
+
+    if os.path.exists("servidor") == True:          #compruebo que existe carpeta
+        ruta= os.getcwd()+"\servidor"
+        if len(os.listdir(ruta))== 0:             #compruebo si esta vacia
+            print("carpeta vacia")
+        else:
+
+            while True:
+                conexion,addr = mi_socket.accept() 
+                print ("Nueva conexión establecida")
+                print (addr)
+
+                file = open ("Prueba.zip","rb")
+                content = file.read (1024)
+
+                while True:
+                    conexion.send(content)      #envio contenido
+                    content = file.read (1024)
+                    conexion.send("descarga completa")
+                break
+
+            conexion.close()
+            file.close()
+            print ("El archivo se ha enviado exitosamente")
+
+    else:
+        print("Crapeta servidor no existe, cree la carpeta y llamela 'servidor' ")
+
 if __name__ == "__main__":
     main()
